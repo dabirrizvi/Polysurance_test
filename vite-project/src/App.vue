e73a62ec5d4338c8b6b5caf54af26a4623ac8439
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2024-02-05 23:21:25 +0000

    importing components to the app

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <h1>test</h1>
+<!-- Display the Intro component -->
+<Intro />
+<!-- Display the Test_1 component -->
+<Test_1 />
 </template>
-<script></script>
-<style scoped>
+
+<script setup>
+// Importing components for the App
+import Intro from "./components/Polysurance_intro.vue"
+import Test_1 from "./components/Test1.vue"
+</script>
+<style>
 </style>
